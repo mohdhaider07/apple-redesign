8453a50b1b18e8ffee8cd00187f0628d9673340b
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2022-11-16 22:52:39 +0530

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -8,7 +8,7 @@ export const config = {
 	apiVersion: "2021-10-21", // Learn more: https://www.sanity.io/docs/api-versioning
 	useCdn: process.env.NODE_ENV === "production",
 };
-
+ 
 // Set up the client for fetching data in the getProps page functions
 export const sanityClient = createClient(config);
 
