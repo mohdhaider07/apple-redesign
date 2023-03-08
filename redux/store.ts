14489a259c5edf925d75f8a2a6b816bc5f91127a
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2023-03-08 22:56:09 +0530

    Update store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import basketSlice   from './basketSlice'
-// Store basket
+
 export const store = configureStore({
   reducer: {
     basket: basketSlice,
